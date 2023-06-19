8b5b29447815d00ccee6c0f1493381294f3b7e31
yeshreddy <yeshwanth2900@gmail.com>
2023-06-19 21:19:56 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+// Creating Html Element and appending it to root
+
+// 1. Using js
+
+// let root = document.getElementById('root')
+// let h1 = document.createElement('h1');
+// h1.innerText="Using JS";
+// root.appendChild(h1);
+// console.log(h1);
+
+
+// 2. Using react
+
+// To use it i have injected React through CDN script tags
+
+
+
